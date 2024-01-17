49c1f8827a49f5672ae8cd3ab2e295b3df245c7e
TZaidel <8tzaidel@gmail.com>
2024-01-17 10:03:16 +0100

    make components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Description from './Description.jsx'
+import Options from './Options.jsx'
+function App() {
+  
+  return (
+    <>
+      <Description />
+      <Options/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Options.jsx
@@ -0,0 +1,12 @@
+export default function Options(){
+    return (
+        <>
+            <ul>
+            <li>Good</li>
+            <li>Neutral</li>
+            <li>Bad</li>
+            <li>Reset</li>
+            </ul>
+        </>
+    )
+}
